81f2b3e5f25d8a12ff8b8283f35fcf282cc75b67
Marcus Overstreet <tkogiah@gmail.com>
2022-01-02 22:01:50 -0800

    added deck.js file

---
+++ js/deck.js
@@ -0,0 +1,14 @@
+const NAME = ['']
+
+class Deck {
+    constructor(cards) {
+        this.cards = cards
+    }
+}
+
+class Card {
+    constructor(name, value) {
+        this.name = name
+        this.value = value
+    }
+}
