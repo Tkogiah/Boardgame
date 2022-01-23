99342db5f9da9755fa90686364183fe7c32fc286
Marcus Overstreet <tkogiah@gmail.com>
2022-01-23 00:41:24 -0800

    working on feeding the html

--- js/characters.js
+++ js/characters.js
@@ -1,31 +1,72 @@
 import { log } from "./main.js"
 
+
+export function $(id) {
+    if(typeof id === 'string' || id instanceof String) {
+        return document.getElementById(id)
+    }
+    else {
+        return id[0]
+    }
+}
+export function name(id, name) {
+    $(id).innerText = `Name: ${name}` 
+}
+
+let level = 3
+
+export function damage(id, damage) {
+    $(id).innerText = `Damage: ${damage}` 
+}
+export function range(id, range) {
+    $(id).innerText = `Range: ${range}` 
+}
+export function speed(id, speed) {
+    $(id).innerText = `Speed: ${speed}` 
+}
+export function money(id, money) {
+    $(id).innerText = `Money: $${money}` 
+}
+export function photo(id, url) {
+    $(id).style.backgroundImage = `url(${url})`
+}
+export function draw(id, arrayLength) {
+    $(id).innerText = `Draw: (${arrayLength})`
+}
+export function discard(id, arrayLength) {
+    $(id).innerText = `Discard: (${arrayLength})`
+}
+export function enemyTakesDamage(location, damage) {
+     
+}
+
+
 export const characterArray = []
 
-export function createPlayer(playerName) {
-    constructor(playerName)
+export function createPlayer(name, level, speed, range, damage) {
+    
     let player = {
-        name: playerName,
-        img: 'img',
-        attack: 1,
-        range: 1,
-        speed: 1,
-        actions: 0,
-        location: 0,
-        draws: 0,
+        name: name,
+        level: level,
+        speed: 3,
+        range: 4, 
+        damage: 2,
+        money: 0,
+        picture: '/assets/scary-man.jpg',
+        position: 0,
         deck: {
-            draw: [],
-            hand: [],
-            discard: []
+            drawPile:['card one','card one','card one','card one','card one'],
+            handPile:['card two', 'card-two', 'card-two','card-two','card-two'],
+            discardPile: ['card-three']
         },
-        head: '',
-        gloves: '',
-        weapon: '',
-        shoes: '',
-        belt: '',
-        mount: ''
+        remainingAttacks: 0,
+        remainingMovements: 0,
+        remainingDraws: 0,
+    
     }
+
     characterArray.push(player)
+    log(characterArray)
 }
 
 //log(createPlayer('Marcus')) => 'Marcus' needs to be in 'quotes' to make it a string.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import * as board from './board.js';
-import * as characters from './characters.js'
+import * as c from './characters.js'
 import * as mr from './movement-range.js'
 import { showHand } from './playmat.js';
 import * as q from './querySelectors.js'
@@ -8,42 +8,30 @@ export function log(a) {
     console.log(a)
 }
 
-let playerOne = {
-    name:'Marcus',
-    level: 1,
-    speed: 3,
-    range: 5, 
-    damage: 2,
-    money: 0,
-    picture: '/assets/scary-man.jpg',
-    position: 0,
-    deck: {
-        drawPile:['card one','card one','card one','card one','card one'],
-        handPile:['card two', 'card-two', 'card-two','card-two','card-two'],
-        discardPile: ['card-three']
-    },
-    remainingAttacks: 0,
-    remainingMovements: 0,
-    remainingDraws: 0,
 
-}
 
-q.name('name', playerOne.name)
-q.level('level', playerOne.level)
-q.speed('speed', playerOne.speed)
-q.damage('damage', playerOne.damage)
-q.range('range', playerOne.range)
-q.money('money', playerOne.money)
-q.photo('photo', playerOne.picture)
+c.createPlayer('Marcus', 1, 3, 2, 5, '/assets/scary-man.jpg')
+c.createPlayer('Jared', 1, 3, 2, 5, '/assets/scary-man.jpg')
+console.log(c.characterArray)
+let CA = c.characterArray
 
-q.draw('draw', playerOne.deck.drawPile.length)
 
-q.discard('discard', playerOne.deck.discardPile.length)
-mr.fillHighlightRangeArray(playerOne.range, playerOne.position)
-mr.fillHighlightMovementArray(playerOne.speed, playerOne.remainingMovements, playerOne.position)
+q.name('name', CA[0].name)
+q.level('level', CA[0].level)
+q.speed('speed', CA[0].speed)
+q.damage('damage', CA[0].damage)
+q.range('range', CA[0].range)
+q.money('money', CA[0].money)
+q.photo('photo', CA[0].picture)
+
+q.draw('draw', CA[0].deck.drawPile.length)
+
+q.discard('discard', CA[0].deck.discardPile.length)
+mr.fillHighlightRangeArray(CA[0].range, CA[0].position)
+mr.fillHighlightMovementArray(CA[0].speed, CA[0].remainingMovements, CA[0].position)
 // mr.attack(location, health, damage)
-log(mr.highlightRangeArray)
-characters.createEnemy(33)
-log(characters.characterArray)
+//log(mr.highlightRangeArray)
+c.createEnemy(33)
+//log(characters.characterArray)
 //q.enemyTakesDamage(33, 5)
 
