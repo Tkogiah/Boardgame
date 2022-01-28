de4a35efd793da76cc98ec4716dad1b60ce4d4f3
Marcus Overstreet <tkogiah@gmail.com>
2022-01-28 00:25:41 -0800

    added functionality to give money based on player location

--- js/board.js
+++ js/board.js
@@ -2,21 +2,32 @@ import { log } from './main.js';
 import { highlightRangeArray } from './movement-range.js';
 import * as p from './playmat.js'
 
-
-
-
-export const boardSpaces = [];
-
 const board = document.querySelectorAll('.hex');
 
 //EVENTLISTENER TO MAKE MONEY
 board.forEach( e => {
     e.addEventListener('click', function() {
         if(e.classList.contains('red') && e.classList.contains('enemy')) {
-            p.playerArray[0].money +=1
+            if(p.playerArray[0].location >= 1 && p.playerArray[0].location <= 6) {
+                p.playerArray[0].money +=1
+            }
+            else if(p.playerArray[0].location >= 7 && p.playerArray[0].location <= 18) {
+                p.playerArray[0].money +=2
+            }
+            else if(p.playerArray[0].location >= 19 && p.playerArray[0].location <= 36) {
+                p.playerArray[0].money +=3
+            }
+            else if(p.playerArray[0].location >= 37 && p.playerArray[0].location <= 60) {
+                p.playerArray[0].money +=4
+            }
+            else if(p.playerArray[0].location >= 61 && p.playerArray[0].location <= 90) {
+                p.playerArray[0].money +=5
+            }
+            else {
+                p.playerArray[0].money += 0
+            }
             p.displayActivePlayer(0)
         }
-        else {log('not red')}
     })
 })
 
